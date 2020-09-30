7df43e09fb58c9e3b9cd765ce24023268f2666d4
bashpound <eunhackjang@gmail.com>
2020-09-30 15:18:20 +0900

    react router

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Route } from 'react-router-dom'
+import About from './comp/About'
+import Home from './comp/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          안녕하세요. <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
     </div>
   )
 }
