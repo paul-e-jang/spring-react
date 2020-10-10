fb73c525321f39edb6671cf26ea7e60f4217a89d
soominJung0413 <doli061214@gmail.com>
2020-10-10 23:22:19 +0900

    /api/fetchUser  작성
    :)

--- src/main/java/bashpound/marketplace/services/member/MemberDetailsServiceImpl.java
+++ src/main/java/bashpound/marketplace/services/member/MemberDetailsServiceImpl.java
@@ -19,29 +19,27 @@ import bashpound.marketplace.infra.repository.MemberMapper;
 import bashpound.marketplace.services.member.exception.DuplicateMemberException;
 
 @Service
-public class MemberDetailsServiceImpl implements MemberService{
-	
+public class MemberDetailsServiceImpl implements MemberService {
+
 	@Autowired
 	private MemberMapper memberMapper;
-	
+
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
-		
-		Member member = memberMapper
-						.selectByUsername(username);
-		
-		if(member == null) {
-			throw new UsernameNotFoundException("User Not Found " +username);
+
+		Member member = memberMapper.selectByUsername(username);
+
+		if (member == null) {
+			throw new UsernameNotFoundException("User Not Found " + username);
 		}
-		
+
 		List<GrantedAuthority> roles = new ArrayList<>();
 		roles.add(new SimpleGrantedAuthority(member.getRole()));
-		
-		MemberDetails memberDetails = new MemberDetails(member,roles);
+
+		MemberDetails memberDetails = new MemberDetails(member, roles);
 		return memberDetails;
 	}
 
@@ -49,30 +47,32 @@ public class MemberDetailsServiceImpl implements MemberService{
 	public Member processRegister(Member memberDto) {
 
 		Member member = memberMapper.selectByUsername(memberDto.getUsername());
-		if(member != null) {
-			throw new DuplicateMemberException("이미 존재하는 이름 입니다. "+memberDto.getUsername());
+		if (member != null) {
+			throw new DuplicateMemberException("이미 존재하는 이름 입니다. " + memberDto.getUsername());
 		}
 		// 계정 등록
 		memberDto.setPassword(passwordEncoder.encode(memberDto.getPassword()));
-		if(memberMapper.insert(memberDto) == 1) {
+		if (memberMapper.insert(memberDto) == 1) {
 			member = memberMapper.selectByUsername(memberDto.getUsername());
 		}
 		return member;
 	}
-	
+
 	@Override
 	public Member selectByUsername(String username) {
 		return memberMapper.selectByUsername(username);
 	}
-	
+
 	@Override
 	public void regDelivery(Delivery delivery) {
 		memberMapper.regDelivery(delivery);
 	}
 
 	@Override
-	public int processSellerUpdate(AuthenticationToken authenticationToken) {
-		// TODO Auto-generated method stub
-		return 0;
+	public boolean processSellerUpdate(AuthenticationToken authenticationToken) {
+		Member member = (Member)authenticationToken.getPrincipal();
+		int result = memberMapper.update(member.getUsername());
+		return result == 1? true : false ;
 	}
+
 }
